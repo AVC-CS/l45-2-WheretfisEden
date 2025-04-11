4d5c88f92a1989685b8eb11718f17dbf57d13f2e
WheretfisEden <milo.gabriel@sbcglobal.net>
2025-04-11 16:20:10 -0700

    its working but its not returing 5 values

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ import random
 
 def main():
     total = 0
-    numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    numbers = [0]* 100
+    index  = 0
+    
+    while total <= 100:
+        num =  random.randint(1,100)
+        numbers[index] = num
+        total += num
+        index += 1
+        
+    numbers = numbers[:index]
+    for i in range(index -1):
+        total += numbers[i]
+    for n in numbers:
+        print (n, end= ' ')
+    
 
-    print(f'The random values are {numbers}')
+
+    #print(f'The random values are {numbers}')
     print(f'The total is {total}')
 
     ########################################
